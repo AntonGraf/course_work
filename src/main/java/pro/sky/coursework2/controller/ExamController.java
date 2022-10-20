c2e665543ed0442c96e14d93d9ce339742fa6dd2
anton <yout>
2022-10-20 13:08:29 +0600

    Добавляет контроллер ExamController

---
+++ src/main/java/pro/sky/coursework2/controller/ExamController.java
@@ -0,0 +1,27 @@
+package pro.sky.coursework2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.coursework2.entity.Question;
+import pro.sky.coursework2.service.ExaminerService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping
+public class ExamController {
+
+    private final ExaminerService examinerService;
+
+
+    public ExamController(ExaminerService examinerService) {
+        this.examinerService = examinerService;
+    }
+
+    @GetMapping
+    public Collection<Question> getQuestions(@RequestParam int amount) {
+        return examinerService.getQuestions(amount);
+    }
+}
