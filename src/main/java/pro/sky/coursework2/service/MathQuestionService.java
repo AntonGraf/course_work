bb4a386b012d9b998a90f178157fdf9eb60f400d
anton <yout>
2022-10-20 18:55:23 +0600

    Добавляет сервис про работе с математическими вопросами

---
+++ src/main/java/pro/sky/coursework2/service/MathQuestionService.java
@@ -0,0 +1,67 @@
+package pro.sky.coursework2.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import pro.sky.coursework2.entity.Question;
+import pro.sky.coursework2.exception.QuestionAddedException;
+import pro.sky.coursework2.exception.QuestionNotFoundException;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+@Service
+@Component("MathQuestionService")
+public class MathQuestionService implements QuestionService{
+
+    Set<Question> questions;
+
+    public MathQuestionService() {
+        questions = new HashSet<>();
+    }
+
+    @Override
+    public Question add(String question, String answer) {
+        return add(new Question(question, answer));
+    }
+
+    @Override
+    public Question add(Question question) {
+
+        if (questions.add(question)) {
+            return question;
+        } else {
+            throw new QuestionAddedException("Не удалось добавить вопрос \"" + question.getQuestion() + "\"");
+        }
+    }
+
+    @Override
+    public Question remove(Question question) {
+
+        if (questions.remove(question)) {
+            return question;
+        } else {
+            throw new QuestionNotFoundException("Не удалось удалить вопрос \"" + question.getQuestion() + "\"");
+        }
+
+    }
+
+    @Override
+    public Collection<Question> getAll() {
+        return questions;
+    }
+
+    @Override
+    public Question getRandomQuestion() {
+
+        if (getAll().size() == 0) {
+            throw new QuestionNotFoundException("Список вопросов пуст.");
+        }
+
+        Random random = new Random();
+
+        return (Question) getAll().toArray()[random.nextInt(getAll().size())];
+
+    }
+}
