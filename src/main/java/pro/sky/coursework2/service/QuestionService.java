cf975e4694a973f768c6594932f968317678396d
anton <yout>
2022-10-20 11:33:46 +0600

    Создает интерфейс сервиса работа с вопросами

---
+++ src/main/java/pro/sky/coursework2/service/QuestionService.java
@@ -0,0 +1,14 @@
+package pro.sky.coursework2.service;
+
+import pro.sky.coursework2.entity.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+
+    Question add(String question, String answer);
+    Question add(Question question);
+    Question remove(Question question);
+    Collection<Question> getAll();
+    Question getRandomQuestion();
+}
