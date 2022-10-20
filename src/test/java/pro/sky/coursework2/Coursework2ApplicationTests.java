bd849e7658e32de6f277c5d566e1daf3b2816ea7
anton <yout>
2022-10-20 11:25:03 +0600

    Initial commit

---
+++ src/test/java/pro/sky/coursework2/Coursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
