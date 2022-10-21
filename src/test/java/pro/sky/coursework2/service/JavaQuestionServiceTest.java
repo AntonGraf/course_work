e81b6f315ee8c3f040deb8ddcfa076d978b3c55b
anton <yout>
2022-10-21 20:51:26 +0600

    Исправляет тесты для JavaQuestionService с учетом добавления отдельного репозитория

--- src/test/java/pro/sky/coursework2/service/JavaQuestionServiceTest.java
+++ src/test/java/pro/sky/coursework2/service/JavaQuestionServiceTest.java
@@ -1,29 +1,37 @@
 package pro.sky.coursework2.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pro.sky.coursework2.entity.JavaQuestionRepository;
 import pro.sky.coursework2.entity.Question;
-import pro.sky.coursework2.entity.QuestionRepository;
 import pro.sky.coursework2.exception.QuestionAddedException;
 import pro.sky.coursework2.exception.QuestionNotFoundException;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
 
+@ExtendWith(MockitoExtension.class)
 class JavaQuestionServiceTest {
 
-    private static final QuestionRepository questionRepository = new JavaQuestionRepository();
-    private static final QuestionService out = new JavaQuestionService(questionRepository);
-
-    @BeforeEach
-    void clear() {
-        out.getAll().clear();
-    }
+    private static final String QUESTION_STRING = "Что такое объект";
+    private static final String ANSWER = "Объект - это ";
+    @Mock
+    private JavaQuestionRepository questionRepository;
+    @InjectMocks
+    private JavaQuestionService out;
 
     public static Stream<Arguments> provideParamsTest() {
         return Stream.of(
@@ -42,11 +50,20 @@ class JavaQuestionServiceTest {
     @MethodSource("provideParamsTest")
     void add(String question, String answer, Question excepted) {
 
+        lenient().when(questionRepository.getAll()).thenReturn(Set.of());
         assertTrue(out.getAll().isEmpty());
+
+        lenient().when(questionRepository.add(anyString(),anyString())).thenReturn(true);
+        lenient().when(questionRepository.add(any())).thenReturn(true);
         assertEquals(out.add(question, answer), excepted);
-        assertEquals(out.getRandomQuestion(), excepted);
+
+        lenient().when(questionRepository.getAll()).thenReturn(Set.of(
+                new Question(question, answer)
+        ));
         assertEquals(out.getAll().size(), 1);
 
+        lenient().when(questionRepository.add(anyString(),anyString())).thenReturn(false);
+        lenient().when(questionRepository.add(any())).thenReturn(false);
         assertThrows(QuestionAddedException.class, () -> out.add(question, answer));
         assertEquals(out.getAll().size(), 1);
     }
@@ -54,46 +71,42 @@ class JavaQuestionServiceTest {
     @Test
     void remove() {
 
+        lenient().when(questionRepository.getAll()).thenReturn(Set.of());
         assertTrue(out.getAll().isEmpty());
 
-        Question question1 = new Question("Что такое объект","Объект - это ");
-        Question question2 = new Question("Что такое класс","Класс - это ");
-        Question question3 = new Question("Что такое инкапсуляция", "Инкапсуляция - это ");
-        Question question4 = new Question("Что такое инкапсуляция", "Инкапсуляция - это ....");
+        lenient().when(questionRepository.getAll()).thenReturn(Set.of(
+                new Question(QUESTION_STRING, ANSWER)
+        ));
 
-        assertThrows(QuestionNotFoundException.class, () -> out.remove(question2));
+        lenient().when(questionRepository.remove(any())).thenReturn(true);
+        assertEquals(out.remove(new Question(QUESTION_STRING,ANSWER)),
+                new Question(QUESTION_STRING, ANSWER));
 
-        out.add(question1);
-        out.add(question2);
-        out.add(question3);
-        assertThrows(QuestionAddedException.class, () -> out.add(question4));
-
-        assertEquals(out.getAll().size(), 3);
-        assertEquals(out.remove(new Question("Что такое объект","Объект - это ")), question1);
-        assertEquals(out.getAll().size(), 2);
-
-        assertFalse(out.getAll().contains(question1));
-        assertTrue(out.getAll().contains(question2));
-        assertTrue(out.getAll().contains(question3));
-
-        assertThrows(QuestionNotFoundException.class, () -> out.remove(question1));
+        lenient().when(questionRepository.remove(any())).thenReturn(false);
+        assertThrows(QuestionNotFoundException.class, () ->
+                out.remove(new Question(QUESTION_STRING, ANSWER))
+        );
     }
 
     @Test
     void getRandomQuestion() {
 
+        lenient().when(questionRepository.getAll()).thenReturn(Set.of());
         assertTrue(out.getAll().isEmpty());
-
         assertThrows(QuestionNotFoundException.class, out::getRandomQuestion);
 
-        Question question1 = new Question("Что такое объект","Объект - это ");
-        Question question2 = new Question("Что такое класс","Класс - это ");
-        Question question3 = new Question("Что такое инкапсуляция", "Инкапсуляция - это ");
-
-        out.add(question1);
-        out.add(question2);
-        out.add(question3);
-
+        lenient().when(questionRepository.getAll()).thenReturn(getQuestionList());
         assertTrue(out.getAll().contains(out.getRandomQuestion()));
+    }
+
+    private Collection<Question> getQuestionList() {
+
+        Set<Question> questions = new HashSet<>();
+
+        questions.add(new Question("Что такое объект","Объект - это "));
+        questions.add(new Question("Что такое класс","Класс - это "));
+        questions.add(new Question("Что такое инкапсуляция", "Инкапсуляция - это "));
+
+        return questions;
     }
 }
